2cd8882676708d4d25d1817de4f68466732c060c
Carolline Martins <106552762+carollinemv@users.noreply.github.com>
2023-02-01 16:27:49 -0300

    Add files via upload

--- Menu interativo e controle financeiro/Menu interativo e controle financeiro/menu.js
+++ Menu interativo e controle financeiro/Menu interativo e controle financeiro/menu.js
@@ -1,8 +1,8 @@
-let opcao = ""
+let opcao = "5"
 
 do {
     opcao = prompt(
-        "Seja bem-vindo(a)\n" + 
+        "Seja bem-vindo(a)!!\n" + 
         "\nEscolha uma das opções abaixo:" +
         "\n1. Opção Um" +
         "\n2. Opção Dois" +
@@ -11,5 +11,26 @@ do {
         "\n5. Encerrar" 
 
     )
+
+    switch (opcao){
+        case "1":
+            alert("Você escolheu a opção 1")
+            break
+        case "2":
+            alert("Você escolheu a opção 2")
+            break
+        case "3":
+            alert("Você escolheu a opção 3")
+            break  
+        case "4":
+            alert("Você escolheu a opção 4")
+            break
+        case "5":
+            alert("Você escolheu encerrar.")
+            alert("Encerrando...")
+            break
+        default:
+            alert("Opção inválida.")    
+    }
 
 } while (opcao !== "5")
