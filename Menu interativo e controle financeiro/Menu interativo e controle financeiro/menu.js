ee7ffd4578ffe2c48bcaeee763322c8eb1d4763e
Carolline Martins <106552762+carollinemv@users.noreply.github.com>
2023-02-01 16:02:58 -0300

    Add files via upload

---
+++ Menu interativo e controle financeiro/Menu interativo e controle financeiro/menu.js
@@ -0,0 +1,15 @@
+let opcao = ""
+
+do {
+    opcao = prompt(
+        "Seja bem-vindo(a)\n" + 
+        "\nEscolha uma das opções abaixo:" +
+        "\n1. Opção Um" +
+        "\n2. Opção Dois" +
+        "\n3. Opção Três" +
+        "\n4. Opção Quatro" +
+        "\n5. Encerrar" 
+
+    )
+
+} while (opcao !== "5")
